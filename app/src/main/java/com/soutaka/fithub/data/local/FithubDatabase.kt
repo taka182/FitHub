6cc4e96c90ab139c288fea325d5eadf94c022372
Taka <soutakaoka92@gmail.com>
2023-04-09 18:19:01 +0900

    entitiesに間違って指定したBodyMetricsDaoをBodyMetricsEntityに変更。exportSchemaにfalseを指定するよう変更

--- app/src/main/java/com/soutaka/fithub/data/local/FithubDatabase.kt
+++ app/src/main/java/com/soutaka/fithub/data/local/FithubDatabase.kt
@@ -3,8 +3,9 @@ package com.soutaka.fithub.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.soutaka.fithub.data.local.entity.BodyMetricsEntity
 
-@Database(entities = [BodyMetricsDao::class], version = 1)
+@Database(entities = [BodyMetricsEntity::class], version = 1, exportSchema = false)
 @TypeConverters(DateTimeConverter::class)
 abstract class FithubDatabase : RoomDatabase() {
     abstract fun bodyMetricsDao(): BodyMetricsDao
