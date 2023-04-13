26547e0945512f6954bcae99e0e6397dc3a77ccd
Taka <soutakaoka92@gmail.com>
2023-04-13 22:51:35 +0900

    身長・体重入力ダイアログ表示

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.soutaka.fithub.presentation.body_metrics.components.BodyMetricsEditDialog
+import com.soutaka.fithub.presentation.body_metrics.viewmodel.BodyMetricsViewModel
 
 @Composable
 fun BodyMetricsScreen(
     viewModel: BodyMetricsViewModel = hiltViewModel(),
 ) {
+    BodyMetricsEditDialog()
+
     Column() {
         Text(text = viewModel.bodyMetrics.firstOrNull()?.id.toString())
         Text(text = viewModel.bodyMetrics.firstOrNull()?.height.toString())
