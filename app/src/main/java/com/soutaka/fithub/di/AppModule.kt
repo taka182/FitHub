4ab2ba768c30321d94996dccea45e2222a853374
Taka <soutakaoka92@gmail.com>
2023-04-13 19:31:21 +0900

    bodyMetricsDaoをコメントアウト

---
+++ app/src/main/java/com/soutaka/fithub/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.soutaka.fithub.di
+
+import android.content.Context
+import androidx.room.Room
+import com.soutaka.fithub.data.local.FithubDatabase
+import com.soutaka.fithub.data.repository.BodyMetricsRepositoryImpl
+import com.soutaka.fithub.domain.repository.BodyMetricsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, FithubDatabase::class.java, "fithub_database").build()
+
+//    @Provides
+//    @Singleton
+//    fun bodyMetricsDao(db: FithubDatabase) = db.bodyMetricsDao()
+
+    @Provides
+    @Singleton
+    fun bodyMetricsRepository(db: FithubDatabase) : BodyMetricsRepository {
+        return BodyMetricsRepositoryImpl(db.bodyMetricsDao())
+    }
+}
