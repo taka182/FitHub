e714a4810a98088992610cd627daa70102fe67de
Taka <soutakaoka92@gmail.com>
2023-04-15 23:07:15 +0900

    コードの整合

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
@@ -1,13 +1,18 @@
 package com.soutaka.fithub.presentation.body_metrics
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NoteAdd
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.soutaka.fithub.presentation.body_metrics.components.BodyMetricsEditDialog
 import com.soutaka.fithub.presentation.body_metrics.viewmodel.BodyMetricsViewModel
@@ -28,10 +33,10 @@ fun BodyMetricsScreen(
         }){
 }
 
-    Column() {
-        Text(text = viewModel.bodyMetrics.firstOrNull()?.id.toString())
-        Text(text = viewModel.bodyMetrics.firstOrNull()?.height.toString())
-        Text(text = viewModel.bodyMetrics.firstOrNull()?.weight.toString())
-        Text(text = viewModel.bodyMetrics.firstOrNull()?.createdAt.toString())
-    }
+//    Column() {
+//        Text(text = viewModel.bodyMetrics.firstOrNull()?.id.toString())
+//        Text(text = viewModel.bodyMetrics.firstOrNull()?.height.toString())
+//        Text(text = viewModel.bodyMetrics.firstOrNull()?.weight.toString())
+//        Text(text = viewModel.bodyMetrics.firstOrNull()?.createdAt.toString())
+//    }
 }
