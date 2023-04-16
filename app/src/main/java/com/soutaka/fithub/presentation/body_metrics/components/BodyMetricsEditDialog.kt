b6b33d9bfd65d0b8923ef48d1195500487c921fd
Taka <soutakaoka92@gmail.com>
2023-04-16 21:58:49 +0900

    ハードコーディングの箇所を変数に修正。ok,closeボタンの処理を追加

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsEditDialog.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsEditDialog.kt
@@ -5,19 +5,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.soutaka.fithub.R
+import com.soutaka.fithub.presentation.body_metrics.DialogState
 import com.soutaka.fithub.presentation.body_metrics.viewmodel.BodyMetricsViewModel
 import java.time.LocalDate
 
@@ -26,39 +28,51 @@ fun BodyMetricsEditDialog(
     viewModel: BodyMetricsViewModel = hiltViewModel(),
     dialogState: DialogState = DialogState.Close
 ) {
-    val heightState = remember { mutableStateOf("") }
-    val weightState = remember { mutableStateOf("") }
+    var height by remember {
+        mutableStateOf(
+            if (dialogState is DialogState.Edit) dialogState.bodyMetrics.height.toString()
+            else ""
+        )
+    }
+    var weight by remember {
+        mutableStateOf(
+            if (dialogState is DialogState.Edit) dialogState.bodyMetrics.weight.toString()
+            else ""
+        )
+    }
 
     Dialog(
         onDismissRequest = { viewModel.closeDialog() }) {
         Box(
             modifier = Modifier
                 .size(350.dp)
-                .background(Color.White, RoundedCornerShape(50.dp))
+                .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(50.dp))
                 .padding(10.dp),
             contentAlignment = Alignment.Center
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
-                modifier = Modifier
-                    .background(Color.White)
             ) {
-                Text(text = "記録登録")
+                Text(
+                    text = if (dialogState is DialogState.Edit) stringResource(R.string.record_update) else stringResource(
+                        R.string.record_add
+                    )
+                )
                 Spacer(modifier = Modifier.height(10.dp))
                 Text(text = LocalDate.now().toString())
                 PinkLabelTextField(
-                    value = heightState.value,
-                    onValueChange = { newtext -> heightState.value = newtext },
-                    label = "身長(cm)",
+                    value = height,
+                    onValueChange = { newText -> height = newText },
+                    label = stringResource(R.string.height),
                     placeholder = "170"
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Spacer(modifier = Modifier.width(10.dp))
                 PinkLabelTextField(
-                    value = weightState.value,
-                    onValueChange = { newtext -> weightState.value = newtext },
-                    label = "体重(kg)",
+                    value = weight,
+                    onValueChange = { newText -> weight = newText },
+                    label = stringResource(R.string.weight),
                     placeholder = "65"
                 )
                 Spacer(modifier = Modifier.height(16.dp))
@@ -70,19 +84,24 @@ fun BodyMetricsEditDialog(
                     Button(
                         modifier = Modifier.width(120.dp),
                         onClick = {
-
+                            viewModel.closeDialog()
                         },
                     ) {
-                        Text(text = "Close")
+                        Text(text = stringResource(R.string.close_btn))
                     }
                     Spacer(modifier = Modifier.width(10.dp))
                     Button(
                         modifier = Modifier.width(120.dp),
                         onClick = {
-//                                  viewModel.addBodyMetrics()
+                            viewModel.closeDialog()
+                            if (dialogState is DialogState.Edit) {
+                                viewModel.updateBodyMetrics(dialogState.bodyMetrics, height, weight)
+                            } else {
+                                viewModel.addBodyMetrics(height, weight)
+                            }
                         },
                     ) {
-                        Text(text = "OK")
+                        Text(text = stringResource(R.string.ok_btn))
                     }
                 }
             }
