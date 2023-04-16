02d78f119800034f08f371be013976894a3a25a4
Taka <soutakaoka92@gmail.com>
2023-04-16 18:59:47 +0900

    bmiの計算処理を追加

--- app/src/main/java/com/soutaka/fithub/domain/model/BodyMetrics.kt
+++ app/src/main/java/com/soutaka/fithub/domain/model/BodyMetrics.kt
@@ -1,10 +1,14 @@
 package com.soutaka.fithub.domain.model
 
 import java.time.LocalDate
+import kotlin.math.pow
 
 data class BodyMetrics(
     val id: Int,
-    val height: Double,
-    val weight: Double,
+    var height: Double,
+    var weight: Double,
     val createdAt: LocalDate
 )
+
+val BodyMetrics.bmi: Double
+    get() = weight / (height / 100).pow(2)
