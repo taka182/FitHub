df2d0ad14b51a284af8256c86bbacdde517ff0b0
Taka <soutakaoka92@gmail.com>
2023-04-16 21:54:46 +0900

    リストが空の時の画面を作成、編集、削除する際の処理を追加

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/BodyMetricsScreen.kt
@@ -3,7 +3,7 @@ package com.soutaka.fithub.presentation.body_metrics
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NoteAdd
-import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.filled.SearchOff
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -14,29 +14,56 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.soutaka.fithub.R
 import com.soutaka.fithub.presentation.body_metrics.components.BodyMetricsEditDialog
+import com.soutaka.fithub.presentation.body_metrics.components.BodyMetricsList
 import com.soutaka.fithub.presentation.body_metrics.viewmodel.BodyMetricsViewModel
 
 @Composable
 fun BodyMetricsScreen(
     viewModel: BodyMetricsViewModel = hiltViewModel(),
 ) {
-    if (viewModel.isShowDialog){
-        BodyMetricsEditDialog()
+    when (val dialogState = viewModel.dialogState) {
+        is DialogState.Edit, DialogState.Add -> BodyMetricsEditDialog(dialogState = dialogState)
+        is DialogState.Close -> {}
     }
 
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = { viewModel.isShowDialog = true }) {
-                Icon(imageVector = Icons.Default.NoteAdd, contentDescription = "体重記録")
+            FloatingActionButton(onClick = { viewModel.showAddDialog() }) {
+                Icon(imageVector = Icons.Default.NoteAdd, contentDescription = stringResource(R.string.weight_record))
             }
-        }){
+        }) {
+        if (viewModel.bodyMetrics.isEmpty()) {
+            NoDataAvailableUI()
+        } else {
+            BodyMetricsList(
+                bodyMetricsList = viewModel.bodyMetrics,
+                onClickRow = {
+                    viewModel.showEditDialog(it)
+                }
+            ) {
+                viewModel.deleteBodyMetrics(it)
+            }
+        }
+    }
 }
 
-//    Column() {
-//        Text(text = viewModel.bodyMetrics.firstOrNull()?.id.toString())
-//        Text(text = viewModel.bodyMetrics.firstOrNull()?.height.toString())
-//        Text(text = viewModel.bodyMetrics.firstOrNull()?.weight.toString())
-//        Text(text = viewModel.bodyMetrics.firstOrNull()?.createdAt.toString())
-//    }
+@Composable
+fun NoDataAvailableUI() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Default.SearchOff,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(text = stringResource(R.string.no_data))
+        }
+    }
 }
