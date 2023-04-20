1eaaf77419713af391ccd5e590330d27dd0e2ad9
Taka <soutakaoka92@gmail.com>
2023-04-20 21:11:56 +0900

    userDaoの追加

--- app/src/main/java/com/soutaka/fithub/data/local/FithubDatabase.kt
+++ app/src/main/java/com/soutaka/fithub/data/local/FithubDatabase.kt
@@ -5,10 +5,14 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.soutaka.fithub.data.local.converter.DateTimeConverter
 import com.soutaka.fithub.data.local.dao.BodyMetricsDao
+import com.soutaka.fithub.data.local.dao.UserDao
 import com.soutaka.fithub.data.local.entity.BodyMetricsEntity
+import com.soutaka.fithub.data.local.entity.UserEntity
 
-@Database(entities = [BodyMetricsEntity::class], version = 1, exportSchema = false)
+@Database(entities = [BodyMetricsEntity::class, UserEntity::class], version = 1, exportSchema = false)
 @TypeConverters(DateTimeConverter::class)
 abstract class FithubDatabase : RoomDatabase() {
     abstract fun bodyMetricsDao(): BodyMetricsDao
+
+    abstract fun userDao(): UserDao
 }
