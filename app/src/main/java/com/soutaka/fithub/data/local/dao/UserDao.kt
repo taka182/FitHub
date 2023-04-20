25741a078700b849a2fcf1be6efc4d9e1dd7b2db
Taka <soutakaoka92@gmail.com>
2023-04-20 21:28:35 +0900

    UserDaoの作成

---
+++ app/src/main/java/com/soutaka/fithub/data/local/dao/UserDao.kt
@@ -0,0 +1,20 @@
+package com.soutaka.fithub.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.soutaka.fithub.data.local.entity.UserEntity
+
+@Dao
+interface UserDao {
+
+    @Insert
+    suspend fun insertUser(userEntity: UserEntity)
+
+    @Query("SELECT * FROM UserEntity WHERE userId = 1")
+    suspend fun loadUser(): UserEntity?
+
+    @Update
+    suspend fun updateUserEntity(userEntity: UserEntity)
+}
