66129922d8c91f3afe4bbf6ac1344c00564ca991
Taka <soutakaoka92@gmail.com>
2023-04-22 15:11:28 +0900

    BodyMetricsDialogで表示しているTextField

---
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsTextField.kt
@@ -0,0 +1,57 @@
+package com.soutaka.fithub.presentation.body_metrics.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import com.soutaka.fithub.R
+
+@Composable
+fun PinkLabelTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    placeholder: String,
+    isError: Boolean,
+) {
+    Column {
+        Text(
+            text = label,
+            color = Color(0xFFF85F6A),
+            fontWeight = FontWeight.Bold,
+
+            )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = value,
+            onValueChange = onValueChange,
+            isError = isError,
+            supportingText = {
+                if (isError) {
+                    Text(text = stringResource(R.string.body_metrics_text))
+                }
+            },
+            trailingIcon = {
+                if (isError)
+                    Icon(imageVector = Icons.Default.Error, contentDescription = "エラーアイコン")
+            },
+            placeholder =
+            {
+                Text(text = placeholder)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            singleLine = true,
+        )
+    }
+}
