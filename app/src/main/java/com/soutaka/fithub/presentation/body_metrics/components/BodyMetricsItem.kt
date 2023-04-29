ce7ab1d8711abfa4095da53085600b5c57581b9e
Taka <soutakaoka92@gmail.com>
2023-04-29 15:18:41 +0900

    BodyMetricsDeleteDialog表示処理を追加

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsItem.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsItem.kt
@@ -5,29 +5,33 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.WaterDrop
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.soutaka.fithub.R
 import com.soutaka.fithub.domain.model.BodyMetrics
 import com.soutaka.fithub.domain.model.bmi
+import com.soutaka.fithub.presentation.body_metrics.viewmodel.BodyMetricsViewModel
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
 @Composable
 fun BodyMetricsItem(
     bodyMetricsItem: BodyMetrics,
+    viewModel: BodyMetricsViewModel = hiltViewModel(),
     onClickRow: (BodyMetrics) -> Unit,
     onClickDelete: (BodyMetrics) -> Unit,
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+
     OutlinedCard(
         modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp),
-
-        ) {
+    ) {
         Row(
             modifier = Modifier
                 .clickable { onClickRow(bodyMetricsItem) }
@@ -49,9 +53,17 @@ fun BodyMetricsItem(
                     )
                     Spacer(modifier = Modifier.width(150.dp))
                     IconButton(onClick = {
-                        onClickDelete(bodyMetricsItem)
+                        showDialog = true
+//                        onClickDelete(bodyMetricsItem)
                     }) {
                         Icon(imageVector = Icons.Default.WaterDrop, contentDescription = "削除")
+                    }
+                    if (showDialog) {
+                        BodyMetricsDeleteDialog(
+                            bodyMetricsItem = bodyMetricsItem,
+                            onClickDelete = onClickDelete,
+                            onDismissRequest = { showDialog = false }
+                        )
                     }
                 }
                 Divider(modifier = Modifier.padding(vertical = 7.dp))
