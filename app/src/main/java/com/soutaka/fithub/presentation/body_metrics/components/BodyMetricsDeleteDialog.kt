5b4de10187974cbd2348eb86f9815f034653465f
Taka <soutakaoka92@gmail.com>
2023-04-30 00:12:46 +0900

    Iconの表示

--- app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsDeleteDialog.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/body_metrics/components/BodyMetricsDeleteDialog.kt
@@ -1,13 +1,22 @@
 package com.soutaka.fithub.presentation.body_metrics.components
 
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.soutaka.fithub.R
 import com.soutaka.fithub.domain.model.BodyMetrics
+import java.lang.reflect.Modifier
 
 @Composable
 fun BodyMetricsDeleteDialog(
@@ -19,8 +28,15 @@ fun BodyMetricsDeleteDialog(
 
     AlertDialog(
         onDismissRequest = onDismissRequest,
-        title = { Text(text = "test") },
-        text = { Text(text = "test") },
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Default.Error, contentDescription = "エラーアイコン")
+                Text(text = stringResource(R.string.delete_dialog_title))
+            }
+        },
+        text = { Text(text = stringResource(R.string.delete_dialog_text)) },
         confirmButton = {
             TextButton(
                 onClick = {
