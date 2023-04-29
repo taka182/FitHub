c765a2313c2551443b5817902dbf6a8341290d78
Taka <soutakaoka92@gmail.com>
2023-04-29 15:20:34 +0900

    userIdの初期値を削除、isManを追加

--- app/src/main/java/com/soutaka/fithub/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/soutaka/fithub/data/local/entity/UserEntity.kt
@@ -2,12 +2,14 @@ package com.soutaka.fithub.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.LocalDate
 
 @Entity
 data class UserEntity(
-    @PrimaryKey val userId: Int = 1,
-    var name: String,
-    var age: Int,
-    var height: Double,
-    var goalWeight: Double,
+    @PrimaryKey val userId: Int,
+    val name: String,
+    val birthDay: LocalDate,
+    val userHeight: Double,
+    val isMan: Boolean,
+    val goalWeight: Double,
 )
