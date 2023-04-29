62586bc7dfa2b4587774a2aa525f5e60c7995d0e
Taka <soutakaoka92@gmail.com>
2023-04-29 15:19:37 +0900

    メソッドの処理を追加

--- app/src/main/java/com/soutaka/fithub/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/soutaka/fithub/data/repository/UserRepositoryImpl.kt
@@ -1,22 +1,23 @@
 package com.soutaka.fithub.data.repository
 
 import com.soutaka.fithub.data.local.dao.UserDao
+import com.soutaka.fithub.data.mapper.toUserEntity
+import com.soutaka.fithub.data.mapper.toUserProfile
 import com.soutaka.fithub.domain.model.UserProfile
 import com.soutaka.fithub.domain.repository.UserRepository
 
 class UserRepositoryImpl(
     private val userDao: UserDao
 ) : UserRepository {
-
-    override fun getUserProfile(): UserProfile {
-        TODO("Not yet implemented")
+    override suspend fun getUserProfile(): UserProfile? {
+        return userDao.loadUser()?.toUserProfile()
     }
 
-    override fun addUserProfile(userProfile: UserProfile) {
-        TODO("Not yet implemented")
+    override suspend fun addUserProfile(userProfile: UserProfile) {
+        return userDao.insertUser(userProfile.toUserEntity())
     }
 
-    override fun updateUserProfile(userProfile: UserProfile) {
-        TODO("Not yet implemented")
+    override suspend fun updateUserProfile(userProfile: UserProfile) {
+        return userDao.updateUser(userProfile.toUserEntity())
     }
 }
