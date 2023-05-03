621a328a2948533f23409e124f5700117f737fb0
Taka <soutakaoka92@gmail.com>
2023-05-03 10:31:43 +0900

    コードのフォーマット

--- app/src/main/java/com/soutaka/fithub/presentation/navigation/FithubNavGraph.kt
+++ app/src/main/java/com/soutaka/fithub/presentation/navigation/FithubNavGraph.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
